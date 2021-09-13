d00e2af2c4e5993e8df38a6baa147e9829f55bff
Alexandre Lam <alexandrelam@outlook.com>
2021-09-13 10:15:00 +0200

    feat(github action): auto deploy 1st try (#48)

--- nuxt.config.js
+++ nuxt.config.js
@@ -5,6 +5,10 @@ export default {
   // Target: https://go.nuxtjs.dev/config-target
   target: "static",
 
+  router: {
+    base: "/portfolio-v3/",
+  },
+
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     title: "portfolio-v3",
