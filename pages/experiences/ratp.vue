0ff907143e18d7bfac84bd10da3a5397870cfc6e
Alexandre LAM <alexandrelam@outlook.com>
2021-09-18 22:09:35 +0200

    feat(portfolio): added content / image / english dates

--- pages/experiences/ratp.vue
+++ pages/experiences/ratp.vue
@@ -3,7 +3,7 @@
     title="Ratp"
     :isNew="false"
     image="ratp_cover.png"
-    date="mai - août 2020"
+    date="may - august 2020"
     descriptionTitle="Flood crisis management software at RATP"
     descriptionParagraph="The RATP Plan de Prévention contre le Risque Inondation (PPRI), as its name suggests, plans the actions to be taken during a flood. Indeed, in the most extreme case, the flooding of the RATP network could cause massive losses for the company and impact the entire Ile-de-France which would then be deprived of part of public transport, hence the objective of the PPRI unit to develop a new efficient crisis management tool. I joined the PPRI unit from May 11, 2020 to July 31, 2020 as an intern in charge of studies and works. My mission within the PPRI unit was to collect the needs of the various actors and to develop the specifications for this management software."
     :info={
