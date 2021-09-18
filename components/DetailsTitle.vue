18b49642b0a52fdaff74286530ac5087d615cd38
Alexandre Lam <alexandrelam@outlook.com>
2021-09-18 16:55:31 +0200

    Okarito content :flight_departure:  (#57)
    * Create LICENSE
    
    * feat(github action): auto deploy 1st try (#48)
    
    * Update README.md
    
    * Static image test 🔌  (#50)
    
    * Try to make action work 🔨  (#49)
    
    * Create LICENSE
    
    * feat(github action): auto deploy 1st try (#48)
    
    * feat(image): added default image instead of optimizedé
    
    * Remove nuxt image :x: (#53)
    
    * Okarito content :flight_departure: (#56)
    
    * feat(image): added custom image to details componenets
    
    * feat(okarito): added en content
    
    * feat(slot): always display slot

---
+++ components/DetailsTitle.vue
@@ -0,0 +1,41 @@
+<template lang="pug">
+div.wrapper
+  h2 {{title}}
+  p {{description}}
+</template>
+
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.wrapper {
+  display: flex;
+  justify-content: space-between;
+  align-items: top;
+  margin-top: 6.25rem;
+  margin-bottom: 6.25rem;
+
+  h2 {
+    font-weight: bold;
+    font-size: 2rem;
+  }
+
+  p {
+    max-width: 34rem;
+    line-height: 170%;
+    margin: 0;
+  }
+}
+</style>
