801e5b3c2a52cb78f6c72671456e30a03c57352c
Alexandre LAM <alexandrelam@outlook.com>
2021-09-18 21:29:02 +0200

    feat(ratp): added ratp content and iamge

--- pages/experiences/ratp.vue
+++ pages/experiences/ratp.vue
@@ -2,10 +2,10 @@
   Details(
     title="Ratp"
     :isNew="false"
-    image="okarito_details.png"
-    date="mai - août 2021"
-    descriptionTitle="SHAPING THE FUTURE OF PROFESSIONAL TRANPORTATION"
-    descriptionParagraph="Okarito is a Parisian start-up that aims to improve business travel. It offers an application that allows users to make reservations for flights, trains, hotels and rental cars. I worked at Okarito as a frontend developer and was brought to collaborate with the product manager and the different developers of the company. I participated in the development of more than 65 features and bug fixes and added more than 7350 lines of code to the code base. I followed good developer practices, wrote tests with Jest and Cypress. Thanks to this experience, I learned a lot about the software engineering world and improved my skills in Vuejs and Nuxtjs."
+    image="ratp_cover.png"
+    date="mai - août 2020"
+    descriptionTitle="Flood crisis management software at RATP"
+    descriptionParagraph="The RATP Plan de Prévention contre le Risque Inondation (PPRI), as its name suggests, plans the actions to be taken during a flood. Indeed, in the most extreme case, the flooding of the RATP network could cause massive losses for the company and impact the entire Ile-de-France which would then be deprived of part of public transport, hence the objective of the PPRI unit to develop a new efficient crisis management tool. I joined the PPRI unit from May 11, 2020 to July 31, 2020 as an intern in charge of studies and works. My mission within the PPRI unit was to collect the needs of the various actors and to develop the specifications for this management software."
     :info={
       site:"https://www.ratp.fr/",
       timeline:"4 months",
