32f39b6411e0b59a843680f595f079b5d0be389f
Alexandre LAM <alexandrelam@outlook.com>
2021-09-22 22:10:05 +0200

    feat(lesgrainsdesel): added content + link overflow

--- pages/experiences/lesgrainsdesel.vue
+++ pages/experiences/lesgrainsdesel.vue
@@ -5,7 +5,7 @@
     image="lesgrainsdesel_cover.png"
     date="october - february 2021"
     descriptionTitle="EVENT MANAGEMENT FOR A COOPERATIVE SUPERMARKET"
-    descriptionParagraph="The RATP Plan de Prévention contre le Risque Inondation (PPRI), as its name suggests, plans the actions to be taken during a flood. Indeed, in the most extreme case, the flooding of the RATP network could cause massive losses for the company and impact the entire Ile-de-France which would then be deprived of part of public transport, hence the objective of the PPRI unit to develop a new efficient crisis management tool. I joined the PPRI unit from May 11, 2020 to July 31, 2020 as an intern in charge of studies and works. My mission within the PPRI unit was to collect the needs of the various actors and to develop the specifications for this management software."
+    descriptionParagraph="Les Grains de Sel is a cooperative supermarket in Paris. The operation of the supermarket is based on the participation of its members. Les Grains de Sel offers its members to participate in events throughout the week. From this problem of optimizing the organization of these events was born this application. I developed this app with a friend of mine. He took care of the authentication system and I took care of the rest. I made the whole design on Figma and integrated it with Django and PostgreSQL. We also set up Docker to facilitate the launch of the application because of its Odoo backend."
     :info={
       site:"https://www.lesgrainsdesel.fr/",
       git:"https://github.com/alexandrelam/lesgrainsdesel",
