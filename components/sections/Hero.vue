166927c763cdc43aedb597691c342d917826f550
Alexandre LAM <alexandrelam@outlook.com>
2021-10-09 20:55:35 +0200

    feat(animation): stagger animation on hero

--- components/sections/Hero.vue
+++ components/sections/Hero.vue
@@ -11,12 +11,13 @@ export default {
   mounted() {
     this.$nextTick(function () {
       let tl = this.$gsap.timeline();
-      tl.from("#hi", { opacity: 0, y: 300, duration: 0.8 });
-      tl.from(".emoji", { opacity: 0, duration: 1 }, "<0.8");
-      tl.from("#header1", { opacity: 0, y: 300, duration: 1 }, "0.7");
-      tl.from("#header2", { opacity: 0, y: 300, duration: 1 }, "<0.2");
-      tl.from("#lang", { opacity: 0, x: 300, duration: 1 }, "<0.3");
-      tl.from("#git", { opacity: 0, x: 300, duration: 1 }, "<0.2");
+      tl.from(".stagger", 2.4, {
+        opacity: 0,
+        delay: 0.8,
+        ease: "power4.out",
+        skewY: 4,
+        stagger: 0.3,
+      });
     });
   },
   methods: {
@@ -28,18 +29,18 @@ export default {
 <template lang="pug">
 section.hero
   .text-wrapper
-    .hi-wrapper
-      h2#hi HI
+    .hi-wrapper.stagger
+      h2 HI
       IHandWave.emoji
-    h2#header1 I'm Alexandre LAM, a developer
-    h2#header2 based in Paris
-  .icons
-    a#lang(href="#" 
+    h2.stagger I'm Alexandre LAM, a developer
+    h2.stagger based in Paris
+  .icons.stagger
+    a(href="#" 
           @mouseover="setMouseHover(true); setMouseSmall(true)"
           @mouseleave="setMouseHover(false); setMouseSmall(false)"
           )
       span EN
-    a#git(href="https://github.com/alexandrelam"
+    a(href="https://github.com/alexandrelam"
           target="_blank"
           @mouseover="setMouseHover(true); setMouseSmall(true)"
           @mouseleave="setMouseHover(false); setMouseSmall(false)")
