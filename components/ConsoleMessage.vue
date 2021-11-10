f1c73e08c241060a09c410be6219bbbafc60dda8
Alexandre LAM <alexandrelam@outlook.com>
2021-11-10 15:22:15 +0100

    feat: added console message component

---
+++ components/ConsoleMessage.vue
@@ -0,0 +1,18 @@
+<script>
+export default {
+  mounted() {
+    console.log(this.$t("welcome"));
+  },
+};
+</script>
+
+<i18n>
+{
+"fr": {
+  "welcome": "Bienvenue sur mon portfolio"
+},
+"en": {
+  "welcome": "Welcome to my portfolio"
+}
+}
+</i18n>
