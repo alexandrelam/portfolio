d394dc2c01e1df7cc5a9114e7d3b0f302a136947
Alexandre LAM <alexandrelam@outlook.com>
2021-11-11 15:43:37 +0100

    fix: favicon fix

--- nuxt.config.js
+++ nuxt.config.js
@@ -22,7 +22,7 @@ export default {
       { name: "format-detection", content: "telephone=no" },
     ],
     link: [
-      { rel: "icon", type: "image/x-icon", href: "/favicon.ico" },
+      { rel: "icon", type: "image/x-icon", href: "favicon.ico" },
       { rel: "preconnect", href: "https://fonts.googleapis.com" },
       { rel: "preconnect", href: "https://fonts.gstatic.com" },
       {
