2a5c59071cafbc433c3261a106e8cf947f72bc3e
Alexandre LAM <alexandrelam@outlook.com>
2021-11-11 15:28:02 +0100

    feat: css on message + custom lang

--- components/ConsoleMessage.vue
+++ components/ConsoleMessage.vue
@@ -1,7 +1,17 @@
+<template lang="pug">
+</template>
+
 <script>
 export default {
+  computed: {
+    style() {
+      if (this.$i18n.locale == "fr")
+        return "padding: 0.75rem; color: #FFB600; background-color: black; font-size: 1.5rem; font-weight: bold; border-radius:2px;";
+      return "padding: 0.75rem; color: black; background-color: #FFB600; font-size: 1.5rem; font-weight: bold; border-radius:2px;";
+    },
+  },
   mounted() {
-    console.log(this.$t("welcome"));
+    console.log(`%c${this.$t("welcome")}`, this.style);
   },
 };
 </script>
@@ -9,10 +19,10 @@ export default {
 <i18n>
 {
 "fr": {
-  "welcome": "Bienvenue sur mon portfolio"
+  "welcome": "Bienvenue sur mon portfolio 👋"
 },
 "en": {
-  "welcome": "Welcome to my portfolio"
+  "welcome": "Welcome to my portfolio, I hope you have a great day ❤️"
 }
 }
 </i18n>
