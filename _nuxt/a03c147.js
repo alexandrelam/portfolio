0965a964af9bd54569295fb96df2902d27b49ac9
alexandrelam <alexandrelam@outlook.com>
2021-11-16 15:07:46 +0000

    Deploying to gh-pages from @ alexandrelam/portfolio@479d473d6b1b96c73ec258254ac0ef132002b422 🚀

--- _nuxt/a03c147.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[27],{347:function(t,o,e){"use strict";e.r(o),o.default=[{title:"okarito",isNew:!0,image:"okarito.png",priority:10},{title:"ratp",isNew:!1,image:"ratp.png",priority:0}]}}]);
